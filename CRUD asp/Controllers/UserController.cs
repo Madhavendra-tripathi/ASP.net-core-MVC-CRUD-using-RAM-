ab9c582cfedd369acbc42b7cae9cc475d8766c1c
Madhav <abc.@xyz.com>
2025-03-29 17:21:20 +0530

    all

---
+++ CRUD asp/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD_asp.Models;
+using System.Linq;
+
+namespace CRUD_asp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User updatedUser)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == updatedUser.Id);
+            if (user != null && ModelState.IsValid)
+            {
+                user.Username = updatedUser.Username;
+                user.Email = updatedUser.Email;
+                user.Password = updatedUser.Password;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(updatedUser);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD asp/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_asp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } // This creates "Users" table
+    }
+}
